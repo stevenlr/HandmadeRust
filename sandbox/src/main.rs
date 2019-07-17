77a67149237e576ec011c8c96382942e07e65996
Steven Le Rouzic <steven.lerouzic@gmail.com>
2019-07-18 00:49:18 +0200

    [serde] Implement more CBOR deserializer, only map remaining!

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 
-use core::mem::transmute;
+use core::{convert::TryInto, mem::transmute};
+use fnd::{
+    alloc::{Allocator, GlobalAllocator},
+    containers::{Array, String},
+};
 use std::{
     io::{Bytes, Cursor, Read, Write},
     vec::Vec,
@@ -10,6 +14,89 @@ mod impls;
 mod traits;
 
 use traits::*;
+
+struct PeekOneReader<R>
+{
+    r: R,
+    peek: Option<std::io::Result<u8>>,
+}
+
+impl<R: Read> PeekOneReader<R>
+{
+    fn new(r: R) -> Self
+    {
+        Self { r, peek: None }
+    }
+
+    fn peek_one(&mut self) -> &std::io::Result<u8>
+    {
+        if self.peek.is_none()
+        {
+            let mut b = [0u8; 1];
+            self.peek = match self.r.read_exact(&mut b)
+            {
+                Ok(_) => Some(Ok(b[0])),
+                Err(e) => Some(Err(e)),
+            };
+        }
+
+        return self.peek.as_ref().unwrap();
+    }
+}
+
+impl<R: Read> Read for PeekOneReader<R>
+{
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize>
+    {
+        if buf.len() == 0
+        {
+            return Ok(0);
+        }
+
+        let already_read = if self.peek.is_some()
+        {
+            let result = self.peek.take().unwrap()?;
+            buf[0] = result;
+            buf = &mut buf[1..];
+            1
+        }
+        else
+        {
+            0
+        };
+
+        Ok(already_read + self.r.read(buf)?)
+    }
+}
+
+#[test]
+fn peek_one()
+{
+    let mut buffer = [0u8; 8];
+    let reader: &[u8] = &[1, 2, 3, 4, 5, 6];
+    let mut reader = PeekOneReader::new(reader);
+
+    assert_eq!(*reader.peek_one().as_ref().unwrap(), 1u8);
+    assert_eq!(*reader.peek_one().as_ref().unwrap(), 1u8);
+
+    reader.read_exact(&mut buffer[0..2]).unwrap();
+    assert_eq!(&buffer[0..2], &[1, 2]);
+
+    assert_eq!(*reader.peek_one().as_ref().unwrap(), 3u8);
+
+    reader.read_exact(&mut buffer[0..1]).unwrap();
+    assert_eq!(&buffer[0..1], &[3]);
+
+    reader.read_exact(&mut buffer[0..2]).unwrap();
+    assert_eq!(&buffer[0..2], &[4, 5]);
+
+    assert_eq!(*reader.peek_one().as_ref().unwrap(), 6u8);
+
+    reader.read_exact(&mut buffer[0..1]).unwrap();
+    assert_eq!(&buffer[0..1], &[6]);
+
+    assert!(reader.peek_one().is_err());
+}
 
 struct CborSerializer<W>
 {
@@ -25,6 +112,7 @@ struct CborCompoundSerializer<'a, W>
 struct CborArraySerializer<'a, W>(CborCompoundSerializer<'a, W>);
 struct CborMapSerializer<'a, W>(CborCompoundSerializer<'a, W>);
 
+#[derive(PartialEq)]
 enum MajorType
 {
     UnsignedInteger,
@@ -392,11 +480,12 @@ enum CborDeserializeError
     UnexpectedType,
     IntegerOverflow,
     VisitorError,
+    Utf8Error,
 }
 
 struct CborDeserializer<R>
 {
-    r: Bytes<R>,
+    r: PeekOneReader<R>,
 }
 
 impl<R: Read> CborDeserializer<R>
@@ -404,21 +493,33 @@ impl<R: Read> CborDeserializer<R>
     #[inline]
     fn read_byte(&mut self) -> Result<u8, CborDeserializeError>
     {
+        let mut b = [0u8; 1];
+        self.read_bytes(&mut b)?;
+        return Ok(b[0]);
+    }
+
+    #[inline]
+    fn peek_byte(&mut self) -> Result<u8, CborDeserializeError>
+    {
         self.r
-            .next()
-            .ok_or(CborDeserializeError::UnexpectedEof)?
-            .map_err(|_| CborDeserializeError::ReadError)
+            .peek_one()
+            .as_ref()
+            .map(|r| *r)
+            .map_err(|e| match e.kind()
+            {
+                std::io::ErrorKind::UnexpectedEof => CborDeserializeError::UnexpectedEof,
+                _ => CborDeserializeError::ReadError,
+            })
     }
 
     #[inline]
     fn read_bytes(&mut self, bytes: &mut [u8]) -> Result<(), CborDeserializeError>
     {
-        for b in bytes.iter_mut()
+        self.r.read_exact(bytes).map_err(|e| match e.kind()
         {
-            *b = self.read_byte()?;
-        }
-
-        Ok(())
+            std::io::ErrorKind::UnexpectedEof => CborDeserializeError::UnexpectedEof,
+            _ => CborDeserializeError::ReadError,
+        })
     }
 
     fn read_integer_from_stream(&mut self, count: usize) -> Result<u64, CborDeserializeError>
@@ -436,6 +537,81 @@ impl<R: Read> CborDeserializer<R>
         return Ok(value);
     }
 
+    fn read_integer_value(&mut self, payload: u8) -> Result<u64, CborDeserializeError>
+    {
+        if payload <= 23
+        {
+            Ok(payload as u64)
+        }
+        else if payload == 24
+        {
+            Ok(self.read_integer_from_stream(1)?)
+        }
+        else if payload == 25
+        {
+            Ok(self.read_integer_from_stream(2)?)
+        }
+        else if payload == 26
+        {
+            Ok(self.read_integer_from_stream(4)?)
+        }
+        else if payload == 27
+        {
+            Ok(self.read_integer_from_stream(8)?)
+        }
+        else
+        {
+            Err(CborDeserializeError::CorruptedHeader)
+        }
+    }
+
+    fn read_byte_string<A: Allocator>(
+        &mut self,
+        vec: &mut Array<u8, A>,
+    ) -> Result<(), CborDeserializeError>
+    {
+        let header = self.read_byte()?;
+
+        let (major_type, payload) =
+            MajorType::from_raw(header).ok_or(CborDeserializeError::CorruptedHeader)?;
+
+        if major_type != MajorType::ByteString
+        {
+            return Err(CborDeserializeError::UnexpectedType);
+        }
+
+        let len = self.read_integer_value(payload)? as usize;
+
+        vec.clear();
+        vec.resize(len, 0);
+
+        return self.read_bytes(&mut *vec);
+    }
+
+    fn read_string<A: Allocator>(&mut self, a: A) -> Result<String<A>, CborDeserializeError>
+    {
+        let header = self.read_byte()?;
+
+        let (major_type, payload) =
+            MajorType::from_raw(header).ok_or(CborDeserializeError::CorruptedHeader)?;
+
+        if major_type != MajorType::TextString
+        {
+            return Err(CborDeserializeError::UnexpectedType);
+        }
+
+        let len = self.read_integer_value(payload)? as usize;
+
+        let mut array = Array::new_with(a);
+        array.clear();
+        array.resize(len, 0);
+        self.read_bytes(&mut *array)?;
+
+        return array
+            .try_into()
+            .map_err(|_| CborDeserializeError::Utf8Error);
+    }
+
     fn read_integer(&mut self) -> Result<(bool, u64), CborDeserializeError>
     {
         let header = self.read_byte()?;
@@ -450,30 +626,7 @@ impl<R: Read> CborDeserializer<R>
             _ => return Err(CborDeserializeError::UnexpectedType),
         };
 
-        let value = if payload <= 23
-        {
-            payload as u64
-        }
-        else if payload == 24
-        {
-            self.read_integer_from_stream(1)?
-        }
-        else if payload == 25
-        {
-            self.read_integer_from_stream(2)?
-        }
-        else if payload == 26
-        {
-            self.read_integer_from_stream(4)?
-        }
-        else if payload == 27
-        {
-            self.read_integer_from_stream(8)?
-        }
-        else
-        {
-            return Err(CborDeserializeError::CorruptedHeader);
-        };
+        let value = self.read_integer_value(payload)?;
 
         return Ok((is_negative, value));
     }
@@ -489,15 +642,14 @@ impl<R: Read> CborDeserializer<R>
         {
             (MajorType::FloatingPoint, F32) =>
             {
-                let mut bytes = [0u8; 4];
-                self.read_bytes(&mut bytes[..])?;
-                Ok(unsafe { transmute::<u32, f32>(u32::from_be_bytes(bytes)) } as f64)
+                Ok(
+                    unsafe { transmute::<u32, f32>(self.read_integer_from_stream(4)? as u32) }
+                        as f64,
+                )
             }
             (MajorType::FloatingPoint, F64) =>
             {
-                let mut bytes = [0u8; 8];
-                self.read_bytes(&mut bytes[..])?;
-                Ok(unsafe { transmute::<u64, f64>(u64::from_be_bytes(bytes)) } as f64)
+                Ok(unsafe { transmute::<u64, f64>(self.read_integer_from_stream(8)?) })
             }
             _ => Err(CborDeserializeError::UnexpectedType),
         }
@@ -529,6 +681,42 @@ impl<R: Read> CborDeserializer<R>
         {
             (MajorType::Special, NULL) => Ok(()),
             _ => Err(CborDeserializeError::UnexpectedType),
+        }
+    }
+
+    fn read_array_length(&mut self) -> Result<Option<usize>, CborDeserializeError>
+    {
+        let header = self.read_byte()?;
+
+        let (major_type, payload) =
+            MajorType::from_raw(header).ok_or(CborDeserializeError::CorruptedHeader)?;
+
+        match (major_type, payload)
+        {
+            (MajorType::Array, 0x1f) => Ok(None),
+            (MajorType::Array, _) => Ok(Some(self.read_integer_value(payload)? as usize)),
+            _ => Err(CborDeserializeError::UnexpectedType),
+        }
+    }
+
+    fn read_break_maybe(&mut self) -> Result<bool, CborDeserializeError>
+    {
+        let header = self.peek_byte()?;
+
+        let (major_type, payload) = match MajorType::from_raw(header)
+        {
+            Some(pair) => pair,
+            None => return Ok(false),
+        };
+
+        match (major_type, payload)
+        {
+            (MajorType::Special, BREAK) =>
+            {
+                self.read_byte()?; // Consume the break byte
+                Ok(true)
+            }
+            _ => Ok(false),
         }
     }
 }
@@ -689,22 +877,34 @@ impl<'de, R: Read> Deserializer<'de> for &mut CborDeserializer<R>
 
     fn deserialize_str<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
-        unimplemented!();
+        let string = self.read_string(GlobalAllocator)?;
+        v.accept_str(&string)
+            .map_err(|_| CborDeserializeError::VisitorError)
     }
 
     fn deserialize_string<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
-        unimplemented!();
+        let string = self.read_string(GlobalAllocator)?;
+        v.accept_string(string)
+            .map_err(|_| CborDeserializeError::VisitorError)
     }
 
     fn deserialize_bytes<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
-        unimplemented!();
+        let mut array = Array::new();
+        self.read_byte_string(&mut array)?;
+        return v
+            .accept_bytes(&array)
+            .map_err(|_| CborDeserializeError::VisitorError);
     }
 
     fn deserialize_bytes_array<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
-        unimplemented!();
+        let mut array = Array::new();
+        self.read_byte_string(&mut array)?;
+        return v
+            .accept_bytes_array(array)
+            .map_err(|_| CborDeserializeError::VisitorError);
     }
 
     fn deserialize_null<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
@@ -716,12 +916,62 @@ impl<'de, R: Read> Deserializer<'de> for &mut CborDeserializer<R>
 
     fn deserialize_array<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
-        unimplemented!();
+        let array_len = self.read_array_length()?;
+
+        let mut accessor = CborDeserializerArrayAccessor {
+            de: self,
+            size: array_len,
+        };
+
+        v.accept_array(accessor)
+            .map_err(|_| CborDeserializeError::VisitorError)
     }
 
     fn deserialize_map<V: Visitor<'de>>(self, v: V) -> Result<V::Value, Self::Err>
     {
         unimplemented!();
+    }
+}
+
+struct CborDeserializerArrayAccessor<'a, R>
+{
+    de: &'a mut CborDeserializer<R>,
+    size: Option<usize>,
+}
+
+impl<'a, 'de, R: Read> ArrayAccessor<'de> for CborDeserializerArrayAccessor<'a, R>
+{
+    type Err = CborDeserializeError;
+
+    fn size_hint(&self) -> Option<usize>
+    {
+        self.size
+    }
+
+    fn next_element<T>(&mut self) -> Result<Option<T>, Self::Err>
+    where
+        T: Deserialize<'de>,
+    {
+        if self.size.is_some()
+        {
+            if self.size.unwrap() > 0
+            {
+                self.size.replace(self.size.unwrap() - 1);
+                T::deserialize(&mut *self.de).map(|x| Some(x))
+            }
+            else
+            {
+                Ok(None)
+            }
+        }
+        else
+        {
+            match self.de.read_break_maybe()?
+            {
+                true => Ok(None),
+                false => T::deserialize(&mut *self.de).map(|x| Some(x)),
+            }
+        }
     }
 }
 
@@ -731,7 +981,7 @@ fn deserialize()
     macro_rules! check_value {
         ($value:expr, $type:ident, $serialized:expr) => {
             let mut de = CborDeserializer {
-                r: $serialized.bytes(),
+                r: PeekOneReader::new(Cursor::new($serialized)),
             };
             let value = $type::deserialize(&mut de).unwrap();
             assert_eq!(value, $value);
@@ -769,13 +1019,131 @@ fn deserialize()
     check_value!(0.0f32, f32, [250, 0, 0, 0, 0]);
     check_value!(1.0f64, f64, [251, 63, 240, 0, 0, 0, 0, 0, 0]);
 
+    struct MyVisitorError;
+    impl VisitorError for MyVisitorError
+    {
+        fn unexpected_type(t: VisitorType) -> Self
+        {
+            panic!();
+        }
+    }
+
+    {
+        struct BytesCheck;
+
+        impl<'de> Visitor<'de> for BytesCheck
+        {
+            type Value = ();
+            type Err = MyVisitorError;
+
+            fn accept_bytes(self, bytes: &[u8]) -> Result<Self::Value, Self::Err>
+            {
+                assert_eq!(bytes, [1, 5, 10, 16, 7]);
+                Ok(())
+            }
+        }
+
+        let mut de = CborDeserializer {
+            r: PeekOneReader::new(Cursor::new(&[69, 1, 5, 10, 16, 7])),
+        };
+
+        de.deserialize_bytes(BytesCheck).unwrap();
+    }
+
+    macro_rules! check_string {
+        ($reference:expr, $bytes:expr) => {{
+            struct StringCheck;
+
+            impl<'de> Visitor<'de> for StringCheck
+            {
+                type Value = ();
+                type Err = MyVisitorError;
+
+                fn accept_str(self, bytes: &str) -> Result<Self::Value, Self::Err>
+                {
+                    assert_eq!(bytes, $reference);
+                    Ok(())
+                }
+            }
+
+            let mut de = CborDeserializer {
+                r: PeekOneReader::new(Cursor::new($bytes)),
+            };
+
+            de.deserialize_string(StringCheck).unwrap();
+        }};
+    }
+
+    check_string!("", &[0x60]);
+    check_string!("a", &[0x61, 0x61]);
+    check_string!("IETF", &[0x64, 0x49, 0x45, 0x54, 0x46]);
+    check_string!("\u{20ac}¢", &[101, 226, 130, 172, 194, 162]);
+
+    {
+        struct NullCheck
+        {
+            found: bool,
+        }
+
+        impl<'de> Visitor<'de> for &mut NullCheck
+        {
+            type Value = ();
+            type Err = MyVisitorError;
+
+            fn accept_null(self) -> Result<Self::Value, Self::Err>
+            {
+                self.found = true;
+                Ok(())
+            }
+        }
+
+        let mut de = CborDeserializer {
+            r: PeekOneReader::new(Cursor::new(&[0xf6])),
+        };
+
+        let mut checker = NullCheck { found: false };
+        de.deserialize_null(&mut checker).unwrap();
+        assert!(checker.found);
+    }
+
+    macro_rules! check_array {
+        ($reference:expr, $bytes:expr) => {{
+            struct ArrayVisitor;
+
+            impl<'de> Visitor<'de> for ArrayVisitor
+            {
+                type Value = Array<u32>;
+                type Err = MyVisitorError;
+
+                fn accept_array<A>(self, mut accessor: A) -> Result<Self::Value, Self::Err>
+                where
+                    A: ArrayAccessor<'de>,
+                {
+                    let mut array = Array::<u32>::new();
+
+                    while let Some(value) = accessor.next_element().map_err(|_| MyVisitorError)?
+                    {
+                        array.push(value);
+                    }
+
+                    return Ok(array);
+                }
+            }
+
+            let mut de = CborDeserializer {
+                r: PeekOneReader::new(Cursor::new($bytes)),
+            };
+
+            let result = de.deserialize_array(ArrayVisitor).unwrap();
+            assert_eq!(&*result, $reference);
+        }};
+    }
+
+    check_array!((&[] as &[u32]), &[0x80, 8, 9, 7]);
+    check_array!((&[1u32, 2u32, 3u32] as &[u32]), &[131, 1, 2, 3, 4, 5]);
+    check_array!((&[1u32, 2u32, 3u32] as &[u32]), &[159, 1, 2, 3, 255, 4, 5]);
+
     /*
-    check_value!("", &[0x60]);
-    check_value!("a", &[0x61, 0x61]);
-    check_value!("IETF", &[0x64, 0x49, 0x45, 0x54, 0x46]);
-    check_value!("\u{20ac}¢", &[101, 226, 130, 172, 194, 162]);
-    check_value!((&[] as &[u32])[..], &[0x80]);
-    check_value!(&[1, 2, 3][..], &[131, 1, 2, 3]);
 
     {
         v.clear();
@@ -844,6 +1212,21 @@ fn serialize()
     check_value!(false, &[0xf4]);
     check_value!(0.0f32, &[250, 0, 0, 0, 0]);
     check_value!(1.0f64, &[251, 63, 240, 0, 0, 0, 0, 0, 0]);
+
+    {
+        v.clear();
+
+        let cursor = Cursor::new(&mut v);
+        let mut serializer = CborSerializer { write: cursor };
+
+        let mut array_ser = serializer.serialize_array(None).unwrap();
+        array_ser.serialize_element(1u32).unwrap();
+        array_ser.serialize_element(2u32).unwrap();
+        array_ser.serialize_element(3u32).unwrap();
+        array_ser.end();
+
+        assert_eq!(v, &[159, 1, 2, 3, 255]);
+    }
 
     {
         v.clear();
